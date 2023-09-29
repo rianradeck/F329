099d2e6b26a7cb0952eec777b557cbcd66db133a
Rian Radeck <rianradeck3@gmail.com>
2023-09-28 23:02:03 -0300

    grafico sitmetria

--- experimento2/campo.py
+++ experimento2/campo.py
@@ -20,15 +20,16 @@ def smooth(x, y):
 
 	return x_new, y_new
 
+pt = {"clear" : "Limpo", "point" : "Ponta", "circle" : "Aro"}
 def f(exp, idx):
-	x = np.linspace(0, 19, len(df[exp]))
+	x = np.linspace(0, 20, len(df[exp]))
 	y = np.array(df[exp][::-1])
 	y[np.isnan(y)] = 0
 			
 	print(x, y, sep='\n')
 	x, y = smooth(x, y)
 
-	axis.plot(x, y)
+	axis.plot(x, y, label=pt[exp])
 
 figure, axis = plt.subplots()
 
@@ -39,8 +40,12 @@ f("circle", 2)
 # for i in range(3):
 # axis.set_aspect('equal', 'box')
 axis.set_xticks(np.arange(0, 20, 1))
+axis.set_yticks(np.arange(0, 4.5, 0.5))
 axis.grid(alpha=.3)
-
+axis.legend(prop={'size':20})
+axis.set_xlabel("Y (cm)")
+axis.set_ylabel("Pot (V)")
+axis.set_title("Potencial ao longo do eixo de simetria")
 
 
 plt.show()
